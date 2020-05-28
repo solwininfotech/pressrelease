6ef38baa6050805e36eb79c2b87d3640a1ed10d2
solwininfotech <info@solwininfotech.com>
2020-05-28 19:35:06 +0530

    Initial commit

---
+++ registration.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Press Release Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_Pressrelease
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+?>
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_Pressrelease',
+    __DIR__
+);
